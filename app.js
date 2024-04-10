c46b5df5d2f2e5d8564857cfb65537fd9825396d
hammtah <115116715+hammtah@users.noreply.github.com>
2024-04-10 14:41:44 +0000

    make navigation without refreshing the page using html hrefs

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 function updateRoute(){
     const path=window.location.pathname;
     const route=routes[path];
+    if(!route) navigate('/error');
     const template=document.getElementById(route.templateId);
     const app=document.getElementById("app");
     const view=template.content.cloneNode(true);//creating a copy of the template content
     app.innerHTML='';
     app.appendChild(view);
 }
-
+function navigate(path){
+    history.pushState({},path,path);//change url
+    updateRoute(path);
+}
+function handleLinks(event){
+    event.preventDefault();
+    navigate(event.target.pathname)
+}
 const routes={
     '/login':{
         templateId:'login'
     },
     '/dashboard':{
         templateId:'dashboard'
+    },
+    '/error':{
+        templateId:'error'
     }
     
 }
+// change template based on a path
 updateRoute();
